b85f1a32599c7ae2020e4a6b634bf1eb9bde2ef2
Flokami <ferrereflorent@hotmail.fr>
2018-03-30 12:24:19 +0400

    Iphone 8 img + menu grand commencé

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-function menuresponsive(){
-	var x = document.getElementById("myMenu")
-	if(x.className === "menu"){
+
+/* Open */
+function openNav() {
+    document.getElementById("myMenu").style.height = "100%";
+    var x = document.getElementById("myMenu")
+    if(x.className === "menu"){
 		x.className += " responsive";
 	}
-	else{
-		x.className = "menu";
-	}
+	var but = document.getElementById('openButton');
+	but.setAttribute("onclick","closeNav()")
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myMenu").style.height = "2.72em";
+    var x = document.getElementById("myMenu")
+    x.className = "menu";
+    var but = document.getElementById('openButton');
+	but.setAttribute("onclick","openNav()")
 }
